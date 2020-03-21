09a141b4c7c743f833151e9e10da13f49c33e290
WeiBoHuang <twer40310@gmail.com>
2020-03-22 00:57:30 +0800

    20200321

---
+++ 變數、常數和類型/ChallengeExercise/Horn_of_Unicorn/src/test/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val hasSteed="nothing"
+    val tavern="Horn of Unicorn"
+    val tavernBoss=""
+    val coin:Int=100
+    val Mead=10
+    val wine=12
+    val LaCroixSparklingWater=15
+
+}
