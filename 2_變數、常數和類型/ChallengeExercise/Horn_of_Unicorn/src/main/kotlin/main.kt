35d18d2047e9da59ea3da027744c3ec91021d7be
WeiBoHuang <twer40310@gmail.com>
2020-03-27 18:25:50 +0800

    20200327

--- 2_變數、常數和類型/ChallengeExercise/Horn_of_Unicorn/src/main/kotlin/main.kt
+++ 2_變數、常數和類型/ChallengeExercise/Horn_of_Unicorn/src/main/kotlin/main.kt
@@ -2,9 +2,6 @@ fun main() {
     val hasSteed="nothing"
     val tavern="Horn of Unicorn"
     val tavernBoss=""
-    val coin:Int=100
-    val Mead=10
-    val wine=12
-    val LaCroixSparklingWater=15
-
+    val coin: Int = 100
+    val manu = listOf('Mead', 'wine', 'LaCroixSparklingWater')
 }
